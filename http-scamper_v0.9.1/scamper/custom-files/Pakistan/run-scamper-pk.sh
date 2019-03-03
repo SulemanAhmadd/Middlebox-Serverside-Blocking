2b41e33a05b31d0bdd240d7b882864d7e30b5cf0
SulemanAhmadd <sulemanahmed54@gmail.com>
2019-03-03 18:15:58 +0500

    added hmtl file flag for domain and shell script update

---
+++ http-scamper_v0.9.1/scamper/custom-files/Pakistan/run-scamper-pk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo '> Configuring IP-table rules...'
+
+sudo iptables -A OUTPUT -p tcp --tcp-flags ALL RST,RST -j DROP
+sudo iptables -A OUTPUT -p tcp --tcp-flags ALL RST,ACK -j DROP
+
+echo '> Starting Scamper Measurements from Pakistan...'
+
+cd ./../../
+
+echo Total nonHTTP-Traceroutes to complete: $(wc -l < custom-files/Pakistan/nonhttp_cmd_file.txt) | tee -a custom-files/Pakistan/log.txt
+echo '> Running scamper...'
+sudo ./scamper -o custom-files/Pakistan/100k-Pakistan-nonhttp.txt -O text -O cmdfile -f custom-files/Pakistan/nonhttp_cmd_file.txt
+wait
+(echo Total nonHTTP-Traceroutes completed: $(cat custom-files/Pakistan/100k-Pakistan-nonhttp.txt | grep -c trace)) | tee -a custom-files/Pakistan/log.txt
+
+echo -e '\n'
+
+echo Total HTTP-Traceroutes to complete: $(wc -l < custom-files/Pakistan/http_cmd_file.txt) | tee -a custom-files/Pakistan/log.txt
+echo '> Running scamper...'
+sudo ./scamper -o custom-files/Pakistan/100k-Pakistan-http.txt -O text -O cmdfile -f custom-files/Pakistan/http_cmd_file.txt
+wait
+echo Total HTTP-Traceroutes completed: $(cat custom-files/Pakistan/100k-Pakistan-http.txt | grep -c trace) | tee -a custom-files/Pakistan/log.txt
+
+echo -e '\n> Pakistan Measurement Complete!'
+
+sudo iptables -D OUTPUT -p tcp --tcp-flags ALL RST,RST -j DROP
+sudo iptables -D OUTPUT -p tcp --tcp-flags ALL RST,ACK -j DROP
+
+echo '> IP-table rules removed'
---
+++ http-scamper_v0.9.1/scamper/custom-files/US/run-scamper-us.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo '> Configuring IP-table rules...'
+
+sudo iptables -A OUTPUT -p tcp --tcp-flags ALL RST,RST -j DROP
+sudo iptables -A OUTPUT -p tcp --tcp-flags ALL RST,ACK -j DROP
+
+echo '> Starting Scamper Measurements from US...'
+
+cd ./../../
+
+echo Total nonHTTP-Traceroutes to complete: $(wc -l < custom-files/US/nonhttp_cmd_file.txt) | tee -a custom-files/US/log.txt
+echo '> Running scamper...'
+sudo ./scamper -o custom-files/US/100k-US-nonhttp.txt -O text -O cmdfile -f custom-files/US/nonhttp_cmd_file.txt
+wait
+(echo Total nonHTTP-Traceroutes completed: $(cat custom-files/US/100k-US-nonhttp.txt | grep -c trace)) | tee -a custom-files/US/log.txt
+
+echo -e '\n'
+
+echo Total HTTP-Traceroutes to complete: $(wc -l < custom-files/US/http_cmd_file.txt) | tee -a custom-files/US/log.txt
+echo '> Running scamper...'
+sudo ./scamper -o custom-files/US/100k-US-http.txt -O text -O cmdfile -f custom-files/US/http_cmd_file.txt
+wait
+echo Total HTTP-Traceroutes completed: $(cat custom-files/US/100k-US-http.txt | grep -c trace) | tee -a custom-files/US/log.txt
+
+echo -e '\n> US Measurement Complete!'
+
+sudo iptables -D OUTPUT -p tcp --tcp-flags ALL RST,RST -j DROP
+sudo iptables -D OUTPUT -p tcp --tcp-flags ALL RST,ACK -j DROP
+
+echo '> IP-table rules removed'
