b3957760fef9da44ffbf94c117b530b07e8ef1f0
shoaibCS <19100080@lums.edu.pk>
2019-05-02 21:45:05 +0500

    Add guide on how to use pipeline and a bug fix

--- DNS_experiment/DNS_pipleline_step2/data.sh
+++ DNS_experiment/DNS_pipleline_step2/data.sh
@@ -3,6 +3,6 @@ cp transfer_to_us_blocked_domains_with_ns.txt ./process_parser/
 cp MDA_DNS_tcpdump_logs.pcap ./process_parser/
 python ./process_parser/get_tashark_logs.py
 python ./process_parser/parse_scamper.py
-country=$( cat ./country.txt )
+country=$( cat /root/country.txt )
 cat ./process_parser/complete_stitched_paths/final_log > $country"_final_log"
 sshpass -p "&AYB&&D#H8#@" scp -o StrictHostKeyChecking=no ./$country"_final_log" root@108.62.49.40:/root/GLOBAL_MDA_DNS_LOGS/
