8110cad314a560dea22387f57ca09e911ed6b776
SulemanAhmadd <sulemanahmed54@gmail.com>
2019-05-06 13:54:35 +0500

    table3 update

--- http-scamper_v0.9.3/scamper/custom-files/Test-DNS/run-meausurement-authservers.py
+++ http-scamper_v0.9.3/scamper/custom-files/Test-DNS/run-meausurement-authservers.py
@@ -7,10 +7,11 @@ import os
 
 def parse_input_line(line):
 	line = line.strip().split()
-	domain = line[0].strip(',')
-	ip = line[1].strip(',')
+	domain = line[0].strip()
+	payload = line[1].strip()
+	ip = line[2].strip()
 
-	return domain, ip
+	return domain, payload, ip
 
 def main():
 	input_filename = sys.argv[1]
@@ -26,10 +27,10 @@ def main():
 	with open('scamper-input-dns.txt', 'wb') as outfile:
 		for dest_addr in inputlist:
 
-			domain, ip = parse_input_line(dest_addr)
+			domain, payload, ip = parse_input_line(dest_addr)
 
 			if ip not in unique_ips:
-				outfile.write('trace -P UDP-Paris -p 34ef010000010000000000000756455253494f4e0442494e440000100003 -d 53 %s\n' % ip)
+				outfile.write('trace -P UDP-Paris -p %s -d 53 %s\n' % (payload, ip))
 				outfile.write('trace -P ICMP-paris %s\n' % (ip))
 				count += 1
 
